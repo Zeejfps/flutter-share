f170b1b5ee5865596889d721e535132782cb5a4f
Zeejfps <Zeejfps@gmail.com>
2018-07-11 11:37:49 -0500

    It works

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,102 +1,61 @@
-// Copyright 2018 Duarte Silveira
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:share/share.dart';
-import 'package:share/receive_share_state.dart';
 
-void main() {
-  runApp(new DemoApp());
+List<CameraDescription> cameras;
+
+Future<Null> main() async {
+  cameras = await availableCameras();
+  runApp(MaterialApp(home: new CameraApp()));
 }
 
-class DemoApp extends StatefulWidget {
+class CameraApp extends StatefulWidget {
   @override
-  DemoAppState createState() => new DemoAppState();
+  _CameraAppState createState() => new _CameraAppState();
 }
 
-class DemoAppState extends ReceiveShareState<DemoApp> {
-  String _text = '';
-  String _shared = '';
+class _CameraAppState extends State<CameraApp> {
+  CameraController controller;
 
   @override
-  void receiveShare(Share shared) {
-    debugPrint("Share received - $shared");
-    setState(() {
-      _shared = shared.toString();
+  void initState() {
+    super.initState();
+    controller = new CameraController(cameras[0], ResolutionPreset.high);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-	enableShareReceiving();
-    return new MaterialApp(
-      title: 'Share Plugin Demo',
-      home: new Scaffold(
-          appBar: new AppBar(
-            title: const Text('Share Plugin Demo'),
-          ),
-          body: new Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Share:',
-                    hintText: 'Enter some text and/or link to share',
-                  ),
-                  maxLines: 2,
-                  onChanged: (String value) => setState(() {
-                        _text = value;
-                      }),
-                ),
-                const Padding(padding: const EdgeInsets.only(top: 24.0)),
-                new Builder(
-                  builder: (BuildContext context) {
-                    return new RaisedButton(
-                      child: const Text('Share'),
-                      onPressed: _text.isEmpty
-                          ? null
-                          : () {
-                              // A builder is used to retrieve the context immediately
-                              // surrounding the RaisedButton.
-                              //
-                              // The context's `findRenderObject` returns the first
-                              // RenderObject in its descendent tree when it's not
-                              // a RenderObjectWidget. The RaisedButton's RenderObject
-                              // has its position and size after it's built.
-                              final RenderBox box = context.findRenderObject();
-                              Share.image(path: "/var/mobile/Containers/Data/Application/DDC011F5-BA6A-4B7D-AFC7-FEA4774894C8/Documents/selfie.png", text: _text).share(
-                                  sharePositionOrigin:
-                                      box.localToGlobal(Offset.zero) &
-                                          box.size);
-//                              Share.image(path: "content://0@media/external/images/media/2129", mimeType: ShareType.TYPE_IMAGE).share(
-//                                  sharePositionOrigin:
-//                                      box.localToGlobal(Offset.zero) &
-//                                          box.size);
-                            },
-                    );
-                  },
-                ),
-                const Padding(padding: const EdgeInsets.only(top: 24.0)),
-                new RaisedButton(
-                  child: const Text('Toggle share receiving'),
-                  onPressed: () {
-                          if (!shareReceiveEnabled) {
-                            enableShareReceiving();
-                          } else {
-                            disableShareReceiving();
-                          }
-                        },
-                ),
-                const Padding(padding: const EdgeInsets.only(top: 24.0)),
-                new Text(_shared),
-              ],
-            ),
-          )),
-    );
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return new Container();
+    }
+    return Scaffold(
+      body: new AspectRatio(
+          aspectRatio:
+          controller.value.aspectRatio,
+          child: new CameraPreview(controller)),
+      floatingActionButton: new FloatingActionButton(onPressed: (){
+        getTemporaryDirectory().then((dir){
+          controller.takePicture(dir.path+"/test.png").then((_){
+            print("Done?");
+            Share.image(path: dir.path+"/test.png", text: "Woo", title: "yey?").share();
+          });
+        });
+      }),
+    );
+  }
 }
